c0875e2952171c9a0468bf981361ae91b80ba59c
Carlos Balladares <carlos@balladares.co>
2018-09-09 16:32:44 -0700

    adding print all substrings code

---
+++ string-manipulation/print-all-substrings.cpp
@@ -0,0 +1,22 @@
+/*
+  Useful code to print all substrings runs O(n^2)
+*/
+
+void print_all_substrings(string str) {
+    int n = str.size();
+    // Pick starting point
+    for (int len = 1; len <= n; len++) {    
+        // Pick ending point
+        for (int i = 0; i <= n - len; i++) {
+            // Print characters from current
+            // starting point to current ending
+            // point.  
+            int j = i + len - 1;            
+            for (int k = i; k <= j; k++) 
+                cout << str[k];
+            cout << endl;
+        }
+
+    }
+    return;
+}
