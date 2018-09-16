c580ebcdba70e2761db31c603fa7a2072ffab34c
Carlos Balladares <carlos@balladares.co>
2018-09-16 15:20:30 -0700

    adding a few useful macros

---
+++ macros/macros.cpp
@@ -0,0 +1,9 @@
+// Printing macros
+#define print_l(a) cout << a << endl;
+#define print(a) cout << a;
+#define endl() cout << endl;
+
+// Max, min macros
+#define MAX(x, y) (((x) > (y)) ? (x) : (y))
+#define MIN(x, y) (((x) < (y)) ? (x) : (y))
+
