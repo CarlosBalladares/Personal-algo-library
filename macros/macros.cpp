4695fb03b53a980da4230cc9d34b092e1bb0b324
Carlos Balladares <carlos@balladares.co>
2018-09-16 18:58:49 -0700

    adding for loop macros

--- macros/macros.cpp
+++ macros/macros.cpp
@@ -1,9 +1,15 @@
 // Printing macros
 #define print_l(a) cout << a << endl;
 #define print(a) cout << a;
+#define print_val(message, val) cout << message << val << endl;
 #define endl() cout << endl;
 
 // Max, min macros
 #define MAX(x, y) (((x) > (y)) ? (x) : (y))
 #define MIN(x, y) (((x) < (y)) ? (x) : (y))
+
+// Loop macros
+#define loop(x, n) for(int x = 0; x < n; x++)
+#define loop_back(x, n) for(int x = n-1 x >= 0; x--)
+
 
