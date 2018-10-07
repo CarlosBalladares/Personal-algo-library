4d75c8e98c344857b34ab1581687e2191873f39d
Carlos Balladares <carlos@balladares.co>
2018-10-07 12:01:19 -0700

    updating competitive programming macros

--- macros/macros.cpp
+++ macros/macros.cpp
@@ -10,16 +10,20 @@
 
 // Loop macros
 #define FOR(x, n) for(int x = 0; x < n; x++)
+#define FOR_IT(iter, iterable) for(auto iter = (iterable).begin(); iter != (iterable).end(); ++iter)
 #define FOR_BACK(x, n) for(int x = n-1 x >= 0; x--)
 
 // debug macros
 #define DEBUG 1
 #define DEBUG_DO(x) if (DEBUG) x
 
-// debug print combined macros
-#define DEBUG 1
+// maros update
+#define DEBUG 0
 #define print_val(message, val) if(DEBUG) cout << message << val << endl;
 #define print_l(a) if(DEBUG) cout << a << endl;
 #define print_vec(a) if(DEBUG) for(int i =0; i <a.size();i++)cout << a[i] <<", "; cout << endl;
-
-
+#define FOR(x, n) for(int x = 0; x < n; x++)
+#define FOR_RANGE(x,beg, n) for(int x = beg; x < n; x++)
+#define FOR_IT(iter, iterable) for(auto iter = (iterable).begin(); iter != (iterable).end(); ++iter)
+#define FOR_BACK(x, n) for(int x = n-1 x >= 0; x--)
+#define WHILE_NE(c) while(!(c.empty()))
